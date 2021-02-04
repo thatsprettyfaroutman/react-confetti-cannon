831723dd6bbcc245747d1cf0fd9e4aed2898b523
thatsprettyfaroutman <viljami.anttonen@gmail.com>
2021-02-04 20:18:12 +0200

    0.4.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { App } from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+export * from './components/Confetti'
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
